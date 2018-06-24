e29ab453eb64305f0d68b1ec3d7f8ce8519efd3b
Zhedong Zheng <zhedongzheng@outlook.com>
2018-06-25 00:51:14 +0100

    clean metrics of link prediction

--- kg-models/link_prediction/model/metrics.py
+++ kg-models/link_prediction/model/metrics.py
@@ -4,61 +4,69 @@ import numpy as np
 import time
 
 
-def subj_next_batch(eval_triples,
-                    entity_to_idx,
-                    predicate_to_idx,
-                    nb_entities):
+def s_next_batch(eval_triples,
+                 entity_to_idx,
+                 predicate_to_idx,
+                 nb_entities,
+                 batch_size):
     for _i, (s, p, o) in tqdm(enumerate(eval_triples), total=len(eval_triples), ncols=70):
         s_idx, p_idx, o_idx = entity_to_idx[s], predicate_to_idx[p], entity_to_idx[o]
         xs = np.arange(nb_entities)
         xp = np.full(shape=(nb_entities,), fill_value=p_idx, dtype=np.int32)
         xo = np.full(shape=(nb_entities,), fill_value=o_idx, dtype=np.int32)
-        yield xs, xp, xo
+        for i in range(0, len(xs), batch_size):
+            yield xs[i: i+batch_size], xp[i: i+batch_size], xo[i: i+batch_size]
 
 
-def obj_next_batch(eval_triples,
+def o_next_batch(eval_triples,
                    entity_to_idx, 
                    predicate_to_idx,
-                   nb_entities):
+                   nb_entities,
+                   batch_size):
     for _i, (s, p, o) in tqdm(enumerate(eval_triples), total=len(eval_triples), ncols=70):
         s_idx, p_idx, o_idx = entity_to_idx[s], predicate_to_idx[p], entity_to_idx[o]
         xs = np.full(shape=(nb_entities,), fill_value=s_idx, dtype=np.int32)
         xp = np.full(shape=(nb_entities,), fill_value=p_idx, dtype=np.int32)
         xo = np.arange(nb_entities)
-        yield xs, xp, xo
+        for i in range(0, len(xs), batch_size):
+            yield xs[i: i+batch_size], xp[i: i+batch_size], xo[i: i+batch_size]
 
 
-def subj_input_fn(eval_triples,
-                  entity_to_idx, 
-                  predicate_to_idx,
-                  nb_entities):
+def s_input_fn(eval_triples,
+               entity_to_idx, 
+               predicate_to_idx,
+               nb_entities,
+               batch_size):
     dataset = tf.data.Dataset.from_generator(
-        lambda: subj_next_batch(eval_triples,
-                                entity_to_idx, 
-                                predicate_to_idx,
-                                nb_entities),
+        lambda: s_next_batch(eval_triples,
+                             entity_to_idx, 
+                             predicate_to_idx,
+                             nb_entities,
+                             batch_size),
         (tf.int32, tf.int32, tf.int32),
-        (tf.TensorShape([nb_entities]),
-         tf.TensorShape([nb_entities]),
-         tf.TensorShape([nb_entities]),))
+        (tf.TensorShape([None,]),
+         tf.TensorShape([None,]),
+         tf.TensorShape([None,]),))
     iterator = dataset.make_one_shot_iterator()
     s, p, o = iterator.get_next()
     return {'s': s, 'p': p, 'o': o}
 
 
-def obj_input_fn(eval_triples,
-                 entity_to_idx, 
-                 predicate_to_idx,
-                 nb_entities):
+def o_input_fn(eval_triples,
+               entity_to_idx, 
+               predicate_to_idx,
+               nb_entities,
+               batch_size):
     dataset = tf.data.Dataset.from_generator(
-        lambda: obj_next_batch(eval_triples,
-                               entity_to_idx, 
-                               predicate_to_idx,
-                               nb_entities),
+        lambda: o_next_batch(eval_triples,
+                             entity_to_idx, 
+                             predicate_to_idx,
+                             nb_entities,
+                             batch_size),
         (tf.int32, tf.int32, tf.int32),
-        (tf.TensorShape([nb_entities]),
-         tf.TensorShape([nb_entities]),
-         tf.TensorShape([nb_entities]),))
+        (tf.TensorShape([None,]),
+         tf.TensorShape([None,]),
+         tf.TensorShape([None,]),))
     iterator = dataset.make_one_shot_iterator()
     s, p, o = iterator.get_next()
     return {'s': s, 'p': p, 'o': o}
@@ -75,54 +83,54 @@ def evaluate_rank(model,
 
     for eval_name, eval_triples in [('valid', valid_triples), ('test', test_triples)]:
         
-        t0 = time.time()
-        _scores_subj = list(model.predict(
-            lambda: subj_input_fn(eval_triples,
-                                  entity_to_idx, 
-                                  predicate_to_idx,
-                                  nb_entities),
-            yield_single_examples = False))
-        print('%.1f secs'%(time.time() - t0))
+        _scores_s = np.fromiter(model.predict(
+            lambda: s_input_fn(eval_triples,
+                               entity_to_idx, 
+                               predicate_to_idx,
+                               nb_entities,
+                               batch_size)),
+            dtype=np.float32,
+            count=len(eval_triples)*nb_entities)
         
-        t0 = time.time()
-        _scores_obj = list(model.predict(
-            lambda: obj_input_fn(eval_triples,
-                                 entity_to_idx, 
-                                 predicate_to_idx,
-                                 nb_entities),
-            yield_single_examples = False))
-        print('%.1f secs'%(time.time() - t0))
+        _scores_o = np.fromiter(model.predict(
+            lambda: o_input_fn(eval_triples,
+                               entity_to_idx, 
+                               predicate_to_idx,
+                               nb_entities,
+                               batch_size)),
+            dtype=np.float32,
+            count=len(eval_triples)*nb_entities)
 
-        Scores_subj = np.reshape(_scores_subj, [len(eval_triples), nb_entities])
-        Scores_obj = np.reshape(_scores_obj, [len(eval_triples), nb_entities])
+        ScoresS = _scores_s.reshape([len(eval_triples), nb_entities])
+        ScoresO = _scores_o.reshape([len(eval_triples), nb_entities])
 
-        ranks_subj, ranks_obj = [], []
-        filtered_ranks_subj, filtered_ranks_obj = [], []
+        ranks_s, ranks_o = [], []
+        filtered_ranks_s, filtered_ranks_o = [], []
 
-        for _i, ((s, p, o), scores_subj, scores_obj) in tqdm(enumerate(zip(eval_triples,
-                                                                           Scores_subj,
-                                                                           Scores_obj)),
-                                                             total=len(eval_triples),
-                                                             ncols=70):
+        for _i, ((s, p, o), scores_s, scores_o) in tqdm(enumerate(zip(eval_triples,
+                                                                      ScoresS,
+                                                                      ScoresO)),
+                                                        total=len(eval_triples),
+                                                        ncols=70):
             s_idx, p_idx, o_idx = entity_to_idx[s], predicate_to_idx[p], entity_to_idx[o]
 
-            ranks_subj += [1 + np.argsort(np.argsort(- scores_subj))[s_idx]]
-            ranks_obj += [1 + np.argsort(np.argsort(- scores_obj))[o_idx]]
+            ranks_s += [1 + np.argsort(np.argsort(- scores_s))[s_idx]]
+            ranks_o += [1 + np.argsort(np.argsort(- scores_o))[o_idx]]
 
-            filtered_scores_subj = scores_subj.copy()
-            filtered_scores_obj = scores_obj.copy()
+            filtered_scores_s = scores_s.copy()
+            filtered_scores_o = scores_o.copy()
 
             rm_idx_s = [entity_to_idx[fs] for (fs, fp, fo) in all_triples if fs != s and fp == p and fo == o]
             rm_idx_o = [entity_to_idx[fo] for (fs, fp, fo) in all_triples if fs == s and fp == p and fo != o]
 
-            filtered_scores_subj[rm_idx_s] = - np.inf
-            filtered_scores_obj[rm_idx_o] = - np.inf
+            filtered_scores_s[rm_idx_s] = - np.inf
+            filtered_scores_o[rm_idx_o] = - np.inf
 
-            filtered_ranks_subj += [1 + np.argsort(np.argsort(- filtered_scores_subj))[s_idx]]
-            filtered_ranks_obj += [1 + np.argsort(np.argsort(- filtered_scores_obj))[o_idx]]
+            filtered_ranks_s += [1 + np.argsort(np.argsort(- filtered_scores_s))[s_idx]]
+            filtered_ranks_o += [1 + np.argsort(np.argsort(- filtered_scores_o))[o_idx]]
 
-        ranks = ranks_subj + ranks_obj
-        filtered_ranks = filtered_ranks_subj + filtered_ranks_obj
+        ranks = ranks_s + ranks_o
+        filtered_ranks = filtered_ranks_s + filtered_ranks_o
 
         for setting_name, setting_ranks in [('Raw', ranks), ('Filtered', filtered_ranks)]:
             mean_rank = np.mean(setting_ranks)
