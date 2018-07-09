d6ed24f2ab6e9cbe6c08e69d9ed5ad13959ca8ae
Zhedong Zheng <zhedongzheng@outlook.com>
2018-07-09 20:31:11 +0100

    minor change

---
+++ kg-models/link_prediction/model/distmult.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import pprint
+
+
+def forward(features, params):
+    e_embed = tf.get_variable('e_embed',
+                              [params['e_vocab_size'], params['embed_dim']],
+                              initializer=tf.contrib.layers.xavier_initializer())
+    p_embed = tf.get_variable('p_embed',
+                              [params['p_vocab_size'], params['embed_dim']],
+                              initializer=tf.contrib.layers.xavier_initializer())
+    
+    s = tf.nn.embedding_lookup(e_embed, features['s'])
+    p = tf.nn.embedding_lookup(p_embed, features['p'])
+    o = tf.nn.embedding_lookup(e_embed, features['o'])
+    
+    logits = tf.reduce_sum(s*p*o, axis=1)
+    
+    return logits
+    
+    
+def model_fn(features, labels, mode, params):
+    logits = forward(features, params)
+    
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        tf.logging.info('\n'+pprint.pformat(tf.trainable_variables()))
+        
+        loss_op = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits,
+                                                                         labels=labels))
+        
+        train_op = tf.train.AdamOptimizer().minimize(
+            loss_op, global_step=tf.train.get_global_step())
+        
+        return tf.estimator.EstimatorSpec(mode = mode,
+                                          loss = loss_op,
+                                          train_op = train_op)
+    
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode, predictions = tf.sigmoid(logits))
